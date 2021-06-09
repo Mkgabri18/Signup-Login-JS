9b5778bfbe5fbf8241ce445b481573fed23f172a
Gabriel Carrasco <gabriel136@hotmail.it>
2021-06-09 03:03:39 +0200

    first commit signinlogin

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# Signup/Login
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/mkgbri18/pen/RwGNwEw](https://codepen.io/mkgbri18/pen/RwGNwEw).
+
+
---
+++ src/script.js
@@ -0,0 +1,45 @@
+const FORM = document.getElementsByTagName("form")[0];
+const FORM_BOX = document.querySelector("img.form-box__image");
+const TITLE = document.querySelector(".form-box__title");
+
+const LOGIN = document.getElementById("login").content;
+const SIGNUP = document.getElementById("signup").content;
+const RECOVERY = document.getElementById("recovery").content;
+
+// ROUTING FUNCTION
+const loginLoad = () => {
+  FORM_BOX.classList.remove("hidden"); // show avatar
+  TITLE.innerHTML = "Login";
+  FORM.innerHTML = "";
+  const clone = LOGIN.cloneNode(true);
+  FORM.appendChild(clone);
+};
+
+const signupLoad = () => {
+  FORM_BOX.classList.add("hidden"); // hidden avatar for signup
+  TITLE.innerHTML = "Signup";
+  FORM.innerHTML = "";
+  const clone = SIGNUP.cloneNode(true);
+  FORM.appendChild(clone);
+};
+
+const recoveryLoad = () => {
+  TITLE.innerHTML = "Recovery";
+  FORM.innerHTML = "";
+  const clone = RECOVERY.cloneNode(true);
+  FORM.appendChild(clone);
+};
+
+// CLEAN INPUT FIELDS
+document.addEventListener("submit", () => {
+  const inputFields = document.querySelectorAll("input");
+  inputFields.forEach((item) => {
+    console.log(item);
+    item.value = item.type === "submit" ? item.value : "";
+  });
+});
+
+// INIT
+document.addEventListener("DOMContentLoaded", () => {
+  loginLoad();
+});
